fcd3cd004c9c90f37861cbd8c6ae38ea92ddcd1a
Abhijeet <abhijeet.eder.io>
2024-04-12 03:19:58 +0530

    connection to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const { chats } = require('./data/data');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const colors = require('colors');
 
 const app = express();
 dotenv.config();
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
@@ -21,4 +24,7 @@ app.get('/api/chat/:id', (req, res) => {
   res.send(singleChat);
 });
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(colors.yellow.bold(`Server started on port ${PORT}`)),
+);
