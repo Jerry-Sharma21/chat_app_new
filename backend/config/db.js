e9377b3329048b38e17535be5ceaf120ba9c3597
Abhijeet <abhijeet.eder.io>
2024-04-13 23:58:18 +0530

    messages api

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,7 @@ const colors = require('colors');
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const connection = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(
       colors.cyan.underline(
